43e00874a04a947a7e5c6898ae7ea10a69eb6e3c
Jack Skiles <jackskiles420@gmail.com>
2021-10-12 18:16:58 -0400

    updated css and some hero image stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import Pantry from "./pantryGrab.jsx";
+// import hcbgImage from "./veggies.png";
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <div>
-                    <div className="heroCont"><img src="./Food.jpg" className="heroCover"></img></div>
+                    <div className="heroCont">
+                        <div  className="heroCover">
+                            <div className="leftColumn">
+                            </div>
+                            <div className="rightColumn">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <Pantry />
             </div>
